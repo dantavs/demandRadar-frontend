3851a4774904119e72c1fdbf934352f729a05ed5
dantavs <dantavs@google.com>
2020-07-12 10:44:32 -0300

    refactor | filter

--- demandRadar.js
+++ demandRadar.js
@@ -12,7 +12,54 @@ async function getIssues() {
 async function listIssues(){
     const res = await getIssues()
     const issues = res.issue
-    console.log('Issues: ', issues)
 
     return issues
 }
+
+async function getIssuesList(viewType){
+
+    const issues = await listIssues()
+
+    let issueItems = ""
+    const issuesList = issues => {
+        issues
+            .filter((issue) => {
+                let status = ""
+                if (viewType == 'radar') {
+                    status = "Em andamento"
+                } else if (viewType == 'entrega') {
+                    status = "Concluído"
+                }
+
+                return issue.status == status
+            })
+            .forEach((issue) => {
+                issueItems += 
+                    `<li class='demand'>
+                        <div class='header'>
+                            <strong>${issue.key}</strong>
+                        </div>
+                        <strong class='epicName'>${issue.epicName}</strong>
+                        <span class='summary'>${issue.summary}</span>
+                        <div class='tags'>
+                            <span class='project'>${issue.project}</span>
+                            <span class='status'>${issue.status}</span>
+                        </div>
+                    </li>`
+            })
+
+        let listElement = document.querySelector('#demandList')
+        listElement.innerHTML = issueItems
+
+}
+
+
+
+    issuesList(issues)
+
+}
+
+    document.querySelector('.getDemandList-form').addEventListener("submit", (e) => {
+        e.preventDefault()
+        getIssuesList(document.querySelector('#demandViewSelect').value)
+    })
