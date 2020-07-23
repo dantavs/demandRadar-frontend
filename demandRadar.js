5ab44d7c7e0401e8222a5a81650de3a9ca8add19
dantavs <dantavs@google.com>
2020-07-23 00:44:52 -0300

    Project Param Added

--- demandRadar.js
+++ demandRadar.js
@@ -1,5 +1,6 @@
-async function getIssues(context) {
-    const url = `http://127.0.0.1:3333/api/v1/getIssuesList/radarSOC`
+async function getIssues(context, project) {
+//    const url = `http://127.0.0.1:3333/api/v1/getIssuesList/radarSOC`
+    const url = `http://127.0.0.1:3333/api/v1/getIssuesListbyContext?project=${project}&id=1`
     try {
         const response = await fetch(url)
         const content = await response.text()
@@ -9,8 +10,8 @@ async function getIssues(context) {
     }
 }
 
-async function listIssues(context){
-    const res = await getIssues(context)
+async function listIssues(context, project){
+    const res = await getIssues(context, project)
     const issues = res.issue
 
     return issues
@@ -31,11 +32,11 @@ function getContext(option){
     return context
 }
 
-async function getIssuesList(viewType){
+async function getIssuesList(viewType, project){
 
     const context = getContext(viewType)
 
-    const issues = await listIssues(context)
+    const issues = await listIssues(context, project)
 
     let issueItems = ""
     const issuesList = issues => {
@@ -75,5 +76,6 @@ async function getIssuesList(viewType){
 
     document.querySelector('.getDemandList-form').addEventListener("submit", (e) => {
         e.preventDefault()
-        getIssuesList(document.querySelector('#demandViewSelect').value)
+        getIssuesList(document.querySelector('#demandViewSelect').value
+                    , document.querySelector('#projectSelect').value)
     })
