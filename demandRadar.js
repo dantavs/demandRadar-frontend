b46338387bf8cb7166bd7bb760384b36830f17eb
dantavs <dantavs@google.com>
2020-07-25 13:21:29 -0300

    layout update with pagination

--- demandRadar.js
+++ demandRadar.js
@@ -32,50 +32,132 @@ function getContext(option){
     return context
 }
 
-async function getIssuesList(viewType, project){
+function getPageItems(items, page){
+    const itemsPerPage = 6
+    const pagesTotal = Math.ceil(items.length / itemsPerPage)
+    let currentPage = page
+    
+    let pageItems = []
 
+    let pageStart = (currentPage-1) * itemsPerPage
+    
+    let pageEnd = 0
+    if (pagesTotal === page){
+        pageEnd = items.length
+    }else{
+        pageEnd = currentPage * itemsPerPage
+    }
+    
+
+    for (var i = pageStart; i < pageEnd; i++){
+        pageItems.push(items[i])
+    }
+    console.log("pageItems: ", pageItems)
+    return pageItems
+}
+
+function setLiItem(issue){
+    const issueItem = `<li class='demand'>
+        <div class='header'>
+            <strong>${issue.key}</strong>
+        </div>
+        <strong class='epicName'>${issue.epicName}</strong>
+        <span class='summary'>${issue.summary}</span>
+        <div class='tags'>
+            <span class='project'>${issue.project}</span>
+            <span class='status'>${issue.status}</span>
+        </div>
+    </li>`
+
+    return issueItem
+}
+
+function setPageItems (issueList){
+    let pageItems = ""
+
+    issueList.forEach((issue) => { 
+            pageItems += setLiItem(issue)
+    })
+
+    return pageItems
+}
+
+function setIssuesList(issues, viewType, page) {
+
+    let filteredIssues = issues
+    .filter((issue) => {
+        let status = ""
+        if (viewType == 'radar') {
+            status = "Em andamento"
+        } else if (viewType == 'entrega') {
+            status = "Concluído"
+        }
+
+        return issue.status == status
+    })
+
+    const pageItems = getPageItems(filteredIssues, page)
+    const pageData = setPageItems(pageItems)
+
+    return pageData
+}
+
+async function getIssuesList(viewType, project, page){
+    console.log("getPage: ", page)
     const context = getContext(viewType)
-
     const issues = await listIssues(context, project)
 
-    let issueItems = ""
-    const issuesList = issues => {
-        issues
-            .filter((issue) => {
-                let status = ""
-                if (viewType == 'radar') {
-                    status = "Em andamento"
-                } else if (viewType == 'entrega') {
-                    status = "Concluído"
-                }
+    const issuesListItems = setIssuesList(issues, viewType, page)
 
-                return issue.status == status
-            })
-            .forEach((issue) => {
-                issueItems += 
-                    `<li class='demand'>
-                        <div class='header'>
-                            <strong>${issue.key}</strong>
-                        </div>
-                        <strong class='epicName'>${issue.epicName}</strong>
-                        <span class='summary'>${issue.summary}</span>
-                        <div class='tags'>
-                            <span class='project'>${issue.project}</span>
-                            <span class='status'>${issue.status}</span>
-                        </div>
-                    </li>`
-            })
+    let listElement = document.querySelector('#demandList')
+    listElement.innerHTML = issuesListItems
 
-        let listElement = document.querySelector('#demandList')
-        listElement.innerHTML = issueItems
+    let pageNumberElement = document.querySelector('#pageNumber')
+    pageNumberElement.innerHTML = page
 
-}
-    issuesList(issues)
+    let pagesTotalElement = document.querySelector('#pagesTotal')
+    pagesTotalElement.innerHTML = 2
 
+    document.querySelector('#prevButton').style.display = "block"
+    document.querySelector('#nextButton').style.display = "block"
+    document.querySelector('#prevButton').disabled = true
 }
 
-    document.querySelector('.getDemandList-form').addEventListener("submit", (e) => {
-        e.preventDefault()
-        getIssuesList(document.querySelector('#demandViewSelect').value
-                    , document.querySelector('#projectSelect').value)
-    })
+
+async function goToNextPage() {
+    console.log("Next Page")
+    await getIssuesList(document.querySelector('#demandViewSelect').value
+                , document.querySelector('#projectSelect').value
+                , parseInt(document.querySelector('#pageNumber').innerHTML) + 1)
+
+    const currentPage = document.querySelector('#pageNumber').innerHTML
+    const pagesTotal = document.querySelector('#pagesTotal').innerHTML
+
+    if (currentPage === pagesTotal){
+        document.querySelector('#nextButton').disabled = true
+    }
+
+    document.querySelector('#prevButton').disabled = false
+}
+
+async function goToPrevPage() {
+    console.log("Prev Page")
+    await getIssuesList(document.querySelector('#demandViewSelect').value
+                , document.querySelector('#projectSelect').value
+                , parseInt(document.querySelector('#pageNumber').innerHTML) - 1)
+
+    const currentPage = document.querySelector('#pageNumber').innerHTML
+
+    if (currentPage === 1){
+        document.querySelector('#prevButton').disabled = true
+    }
+
+    document.querySelector('#nextButton').disabled = false
+}
+
+document.querySelector('.getDemandList-form').addEventListener("submit", (e) => {
+    e.preventDefault()
+    getIssuesList(document.querySelector('#demandViewSelect').value
+                , document.querySelector('#projectSelect').value, 1)
+})
+
